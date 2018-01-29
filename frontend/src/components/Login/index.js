4b85f6cf8f4379578cfac83ab688321639547c6b
raooll <hi@rahulshar.ma>
2018-01-29 15:25:08 +0530

    add login & reset password components

---
+++ frontend/src/components/Login/index.js
@@ -0,0 +1,68 @@
+// src/components/Login/index.js
+import React, { Component } from 'react';
+import { Form, Icon, Input, Button, Checkbox, Layout } from 'antd';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import './style.css';
+
+const { Content } = Layout;
+const FormItem = Form.Item;
+
+class LoginForm extends Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  render() {
+    const { className, ...props } = this.props;
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className={classnames('Login', className)} {...props}>
+        <Content>
+        <Form onSubmit={this.handleSubmit} className="login-form">
+        <FormItem>
+          {getFieldDecorator('email', {
+            rules: [{
+              type: 'email', message: 'The email is not valid!',
+            }, {
+              required: true, message: 'Please enter your Email!',
+            }],
+          })(
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Email" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('password', {
+            rules: [{
+              required: true, message: 'Please enter your password!',
+            }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(
+            <Checkbox>Remember me</Checkbox>
+          )}
+          <a className="login-form-forgot" href="/resetpassword">Forgot password</a>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+        </FormItem>
+      </Form>
+      </Content>
+      </div>
+    );
+  }
+}
+
+export default Form.create()(LoginForm);
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,12 +4,16 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import About from './components/About';
 import NotFound from './components/NotFound';
 import MainView from './components/MainView';
+import Login from './components/Login';
+import ForgotPassword from './components/ForgotPassword';
 import './routes.css'
 
 const Routes = (props) => (
   <Router {...props}>
     <Switch>
       <Route exact path="/" component={MainView} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/resetpassword" component={ForgotPassword} />
       <Route exact path="/about" component={About} />
       <Route path="*" component={NotFound} />
     </Switch>
