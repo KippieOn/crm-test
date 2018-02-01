1ba76893dd450afe6b1aaf7fda80ceb32dd4e642
Hunterx7 <ialbetsram@gmail.com>
2018-02-01 07:22:31 +0000

    some changes on main view

--- frontend/src/components/MainView/index.js
+++ frontend/src/components/MainView/index.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
-import { Layout } from 'antd'
+import { Layout, Menu, Icon, Switch } from 'antd'
 import SideBar from './SideBar'
 import DataTable from './TempData'
+import './style.css'
 
-const { Sider, Content } = Layout;
+const { Header, Sider, Content, Footer } = Layout;
+const SubMenu = Menu.SubMenu;
+
 
 class MainView extends Component {
   state = {
     collapsed: 'false',
+    theme: 'dark',
   }
 
-  componentWillMount = () => {
+  componentWillMount = () => {}
 
+  onCollapse = (collapsed) => {
+    this.setState({ collapsed });
+  }
+
+  changeTheme = (value) => {
+    this.setState({
+      theme: value ? 'dark': 'light',
+    })
   }
 
 
@@ -19,16 +31,54 @@ class MainView extends Component {
     return (
       <Layout>
         <Sider
-           trigger={null}
-           collapsible
-           collapsed={this.state.collapsed}
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+          theme={this.state.theme}
          >
-            <SideBar />
+           <Menu theme={this.state.theme} defaultSelectedKeys={['1']} mode="inline">
+             <Menu.Item key="1">
+               <Icon type="pie-chart" />
+               <span>Option 1</span>
+             </Menu.Item>
+             <Menu.Item key="2">
+               <Icon type="desktop" />
+               <span>Option 2</span>
+             </Menu.Item>
+             <SubMenu
+               key="sub1"
+               title={<span><Icon type="user" /><span>User</span></span>}
+             >
+               <Menu.Item key="3">Tom</Menu.Item>
+               <Menu.Item key="4">Bill</Menu.Item>
+               <Menu.Item key="5">Alex</Menu.Item>
+             </SubMenu>
+             <SubMenu
+               key="sub2"
+               title={<span><Icon type="team" /><span>Team</span></span>}
+             >
+               <Menu.Item key="6">Team 1</Menu.Item>
+               <Menu.Item key="8">Team 2</Menu.Item>
+             </SubMenu>
+             <Menu.Item key="9">
+               <Icon type="file" />
+               <span>File</span>
+             </Menu.Item>
+            </Menu>
+            <Switch
+               checked={this.state.theme === 'dark'}
+               onChange={this.changeTheme}
+               checkedChildren="Dark"
+               unCheckedChildren="Light"
+               className="bottom-switch"
+             />
         </Sider>
         <Layout>
-          <Content>
+         <Header style={{ background: '#fff', padding: 0 }} />
+          <Content className="padding-data minimum-height">
               <DataTable />
           </Content>
+
         </Layout>
     </Layout>
     )
