d1195dfccfe768770b08fc0bb10295c86a7f695e
Hunterx7 <ialbetsram@gmail.com>
2018-02-01 16:44:20 +0000

    Working authentication

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from rest_framework.authentication import (SessionAuthentication,
                                            BasicAuthentication)
 from rest_framework.permissions import IsAuthenticated
@@ -12,8 +13,10 @@ from rest_framework.response import Response
 @authentication_classes((SessionAuthentication, BasicAuthentication))
 @permission_classes((IsAuthenticated))
 def login_view(request, format_none):
+    user = User.objects.get(email=request.user)
+    print(user)
     content = {
-        'user': unicode(request.user),
+        'user': unicode(user.username),
         'auth': unicode(request.auth),
     }
     return Response(content)
