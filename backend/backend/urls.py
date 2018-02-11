ce415a93e0f69f1442692f216dc01fb4c183021a
Hunterx7 <ialbetsram@gmail.com>
2018-02-11 21:43:07 +0000

    added docs

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+from rest_framework import include_docs_urls
 from api import views as crm_api
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls')),
-    url(r'^api/', include('api.urls'))
+    url(r'^api/', include('api.urls')),
+    url(r'^docs/', include_docs_urls(title="crm api")),
 ]
