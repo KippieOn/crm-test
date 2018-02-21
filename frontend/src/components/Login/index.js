eba0f849810346f22a01dded34afbfc6f6b076e0
raooll <hi@rahulshar.ma>
2018-02-21 17:12:59 +0530

    add spinner to login form

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,7 +1,7 @@
 // src/components/Login/index.js
 import React, { Component } from 'react';
 import { Form, Icon, Input, Button, Checkbox, Layout } from 'antd';
-import { Row, Col } from 'antd'
+import { Row, Col, Spin } from 'antd'
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom'
 import classnames from 'classnames';
@@ -96,7 +96,8 @@ class LoginForm extends Component {
             )}
             <a className="login-form-forgot" href="/resetpassword">Forgot password</a>
             <Button type="primary" htmlType="submit" className="login-form-button">
-              Log in
+              { isLoginPending && <Spin size="medium" /> }
+              { !isLoginPending &&  "Log in" }
             </Button>
           </FormItem>
         </Form>
